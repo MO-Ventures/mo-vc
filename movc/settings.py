4943b87d7733a159da14d5ff21cffd21da9a0f9c
Travis <travis@kajoui-MacBook-Pro.local>
2021-08-02 01:51:23 +0900

    Support heroku postgresql

--- movc/settings.py
+++ movc/settings.py
@@ -176,4 +176,6 @@ STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 
 if os.environ.get('Heroku', False):
     import django_heroku
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
     django_heroku.settings(locals())
