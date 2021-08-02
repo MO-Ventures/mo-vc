3346fdf1efa33bfa2201711512aca69f36271450
Travis <travis@kajoui-MacBook-Pro.local>
2021-08-02 18:06:40 +0900

    Remove section and unused class

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView
 from django.utils.translation import gettext as _
-from portfolio.models import Company, Sector, Statistics
+from portfolio.models import Company, Region, Statistics
 from typing import Any, Dict, List
 
 # Create your views here.
@@ -11,27 +11,4 @@ class CompanyListView(ListView):
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['stat'] = Statistics.objects.first
-        context['investment'] = [
-        # Integrity, 
-            {
-                "name": _("Transparency"),
-                "description": _("We value")
-            },
-            {
-                "name": _("Founder"),
-                "description": _("We value")
-            },
-            {
-                "name": _("Responsiblity"),
-                "description": _("We value")
-            },
-            {
-                "name": _("Sustainable"),
-                "description": _("We value")
-            },
-            {
-                "name": _("Profitable"),
-                "description": _("We value")
-            },
-        ]
         return context
