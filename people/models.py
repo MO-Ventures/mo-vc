e2728174524b1089e3db152731584880d030763e
Travis <travis@kajoui-MacBook-Pro.local>
2021-08-02 18:07:44 +0900

    Change layout and make editable

--- people/models.py
+++ people/models.py
@@ -1,11 +1,27 @@
 from adminsortable.models import SortableMixin
 from django.db import models
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Sector(models.TextChoices):
     VC = 'VC', 'VC'
     PE = 'PE', 'PE'
     Management = 'Management', 'Management'
+
+class Introduction(models.Model):
+    heading = models.CharField(max_length=30)
+    subheading = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = "Introduction"
+    
+    def save(self, *args, **kwargs):
+        if self.__class__.objects.count():
+            self.pk = self.__class__.objects.first().pk
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return "Introduction"
 
 class People(SortableMixin):
     the_order = models.PositiveIntegerField(default=0, editable=False, db_index=True)
@@ -14,13 +30,10 @@ class People(SortableMixin):
     position = models.CharField(max_length=20)
     email = models.EmailField()
     photo = models.ImageField(upload_to='people/profile_pics')
-    description = models.TextField()
+    description = HTMLField('description')
 
     class Meta:
         ordering = ["the_order"]
 
     def __str__(self):
         return self.name
-
-    def __unicode__(self):
-        return self.title
--- people/views.py
+++ people/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView
-from people.models import People
+from people.models import Introduction, People
 from typing import Any, Dict
 
 # Create your views here.
@@ -10,4 +10,6 @@ class PeopleListView(ListView):
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['sector'] = set([people.sector for people in People.objects.all()])
+        context['navbar'] = 'text-black'
+        context['intro'] = Introduction.objects.first()
         return context
