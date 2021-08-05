a30061c9bfc7293bd029d40f76502b1cc4130bcb
Travis Cho <chotravis87@gmail.com>
2021-08-06 00:13:28 +0900

    Change secrets

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,7 @@
 import os
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from movc.secrets import KAKAO_API_KEY
+from movc.settings import SECRETS
 
 # Create your views here.
 class ContactView(TemplateView):
@@ -10,6 +10,6 @@ class ContactView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['navbar'] = 'text-black'
-        context["kakao_api_key"] = KAKAO_API_KEY
+        context["kakao_api_key"] = SECRETS['KAKAO_API_KEY']
         return context
     
