3c4543ae9168b57bdff0d4e7e5cedf68ac402e9c
Travis Cho <chotravis87@gmail.com>
2021-08-08 15:37:39 +0900

    Add robots.txt

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="home/index.html")),
+    path('robots.txt/', TemplateView.as_view(template_name="home/robots.txt", content_type="text/plain")),
 ]
