38d03a1c9ce9d33f67a86dcae6b364b2211560dd
Travis Cho <chotravis87@gmail.com>
2021-10-05 20:52:54 +0900

    Moved from decouple to yaml

--- movc/settings.py
+++ movc/settings.py
@@ -13,17 +13,20 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-from decouple import config
+import yaml
 from google.oauth2 import service_account
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+with open(BASE_DIR / 'config.yaml', 'r') as f:
+    config = yaml.load(f, Loader=yaml.SafeLoader)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY')
+SECRET_KEY = config['django']['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False) == 'True'
@@ -50,8 +53,8 @@ INSTALLED_APPS = [
     'people',
     'portfolio',
     'contact',
-    'maintenance'
-    # 'join',
+    'maintenance',
+    'apply',
 ]
 
 MIDDLEWARE = [
@@ -101,10 +104,10 @@ if not DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'HOST': config('DB_HOST'),
-            'NAME': config('DB_NAME'),
-            'USER': config('DB_USER'),
-            'PASSWORD': config('DB_PASSWORD')
+            'HOST': config['database']['host'],
+            'NAME': config['database']['name'],
+            'USER': config['database']['user'],
+            'PASSWORD': config['database']['password'],
         }
     }
 
@@ -153,8 +156,8 @@ LOCALE_PATHS = [ BASE_DIR / 'locale' ]
 STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 
 if not DEBUG:
-    GS_BUCKET_NAME = config('GS_BUCKET_NAME')
-    GS_PROJECT_ID = config('GS_PROJECT_ID')
+    GS_BUCKET_NAME = config['google_cloud']['bucket_name']
+    GS_PROJECT_ID = config['google_cloud']['project_id']
     GS_CREDENTIALS = service_account.Credentials.from_service_account_file(BASE_DIR / 'google_credentials.json')
     GS_LOCATION = 'static'
     GS_DEFAULT_ACL = 'publicRead'
