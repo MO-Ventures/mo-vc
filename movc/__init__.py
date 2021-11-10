f971e9cc0804548b0e5ea9744f466d9f0051bb18
Travis Cho <chotravis87@gmail.com>
2021-11-10 14:02:45 +0900

    Specify sentry environment

--- movc/__init__.py
+++ movc/__init__.py
@@ -1,9 +1,12 @@
+import os
 import sentry_sdk
+
 sentry_sdk.init(
     "https://4c5f291b7e3d4c7ba44594f42a467a50@o945190.ingest.sentry.io/5893779",
 
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    traces_sample_rate=1.0
+    environment='production' if os.environ.get('production') else 'debug',
+    traces_sample_rate=1.0,
 )
